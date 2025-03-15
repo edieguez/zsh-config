8bdc7bad424903738f56377e22375f068a42b870
edieguez <dieguezemmanuel@gmail.com>
2025-03-15 13:16:26 -0600

    Add conditional plugin loading

--- system/bundles.sh
+++ system/bundles.sh
@@ -1,2 +1,10 @@
 # https://sdkman.io/install
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+# Conditional plugin loading
+(( $+commands[aws] )) && plugins+=(aws)
+(( $+commands[fzf] )) && plugins+=(fzf)
+(( $+commands[git] )) && plugins+=(git)
+(( $+commands[go] )) && plugins+=(golang)
+(( $+commands[gradle] )) && plugins+=(gradle)
+(( $+commands[mvn] )) && plugins+=(mvn)
