8df614004fd64919080553e2c7e72ace3ef43381
edieguez <dieguezemmanuel@gmail.com>
2025-06-12 16:04:06 -0600

    Improve FZF search configuration

--- system/bundles.sh
+++ system/bundles.sh
@@ -3,8 +3,21 @@
 
 # Conditional plugin loading
 (( $+commands[aws] )) && plugins+=(aws)
-(( $+commands[fzf] )) && plugins+=(fzf)
 (( $+commands[git] )) && plugins+=(git)
 (( $+commands[go] )) && plugins+=(golang)
 (( $+commands[gradle] )) && plugins+=(gradle)
 (( $+commands[mvn] )) && plugins+=(mvn)
+
+plugins+=(fzf)
+
+if [ $+commands[fzf] ]; then
+  plugins+=(fzf)
+
+  # FZF "**" command syntax
+  # https://github.com/junegunn/fzf/discussions/3922
+  _fzf_compgen_path() {
+    fd --type f --hidden --no-ignore \
+      --exclude Library \
+      --exclude .git
+  }
+fi
