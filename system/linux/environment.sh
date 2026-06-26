0e10cf521dd5b7899022efef9988af04b4aacad6
edieguez <dieguezemmanuel@gmail.com>
2026-06-25 23:49:22 -0600

    Optimize Golang and Rust plugin loading

--- system/linux/environment.sh
+++ system/linux/environment.sh
@@ -1,1 +1,11 @@
 # Declare your Linux related environment variables here
+# Rust configuration
+export RUST_HOME="/opt/rust"
+export CARGO_HOME="$RUST_HOME/cargo"
+export RUSTUP_HOME="$RUST_HOME/rustup"
+
+# Go configuration
+export GOROOT="/opt/go"
+export GOPATH="$GOROOT/gopath"
+
+export CUSTOM_PATH="$CARGO_HOME/bin:$GOROOT/bin:$GOPATH/bin"
